978fb45d508bb6f41f5c51b3a9df0dddd39a4b84
Nick Mironov <mojkugis@gmail.com>
2024-11-21 22:32:14 +0300

    added second experiment

--- a1.cpp
+++ a1.cpp
@@ -4,11 +4,13 @@
 #include <ctime>
 #include <fstream>
 
-std::mt19937 mtRand(0xdeadbeef);
-std::uniform_real_distribution<double> range(0.5, 2.5);
+std::mt19937 mtRand(0xdead);
+std::uniform_real_distribution<double> range1(0.5, 2.5);
+std::uniform_real_distribution<double> range2(0.0, 3.0);
 
 const double TRUE_S = 0.9445171858994637;
-const double TARGET_S = 4.0;
+const double TARGET_S1 = 4.0;
+const double TARGET_S2 = 9.0;
 
 
 bool inside_circle(double x, double y, double x0, double y0, double r2) {
@@ -27,25 +29,36 @@ int main() {
     std::fstream fout("data.csv", std::ios::out);
 
     for (int n = 100; n <= 100000; n += 500) {
-        int corr = 0;
+        int corr1 = 0;
+        int corr2 = 0;
 
         for (int k = 0; k < n; ++k) {
-            double x = range(mtRand);
-            double y = range(mtRand);
+            double x1 = range1(mtRand);
+            double y1 = range1(mtRand);
+
+            double x2 = range2(mtRand);
+            double y2 = range2(mtRand);
 
             //std::cout << x << ' ' << y << '\n';
 
-            if (check_point(x, y)) {
-                ++corr;
+            if (check_point(x1, y1)) {
+                ++corr1;
+            }
+            if (check_point(x2, y2)) {
+                ++corr2;
             }
         } 
 
         //std::cout << corr << '\n';
 
-        double s = TARGET_S * corr / n;
-        double rel_err = std::abs(s - TRUE_S) / TRUE_S;
+        double s1 = TARGET_S1 * corr1 / n;
+        double s2 = TARGET_S2 * corr2 / n;
 
-        fout << n << ',' << s << ',' << rel_err << '\n';
+        double rel_err1 = std::abs(s1 - TRUE_S) / TRUE_S;
+        double rel_err2 = std::abs(s2 - TRUE_S) / TRUE_S;
+
+        fout << n << ',' << s1 << ',' << rel_err1 << ',' <<
+            s2 << ',' <<  rel_err2 << '\n';
     }
 
     return 0;
--- graphs.py
+++ graphs.py
@@ -6,24 +6,35 @@ TRUE_VAL = 0.9445171858994637
 
 data = np.genfromtxt("data.csv", delimiter=',')
 x = data[:, 0]
-res = data[:, 1]
-err = data[:, 2]
+res1 = data[:, 1]
+err1 = data[:, 2]
+res2 = data[:, 3]
+err2 = data[:, 4]
 
 fig, axes = plt.subplots(1, 2, figsize=(12, 5))  # 1 row, 2 columns
 
 # First graph
-axes[0].plot(x, res, color="blue")
+axes[0].plot(x, res1, color="blue", lw=1, 
+             label="Площадь области точек $S_{rec} = 4$")
+axes[0].plot(x, res2, color="gold", lw=1,
+             label="Площадь области точек $S_{rec} = 9$")
 axes[0].axhline(y=TRUE_VAL, linestyle="--", color="black", label="Истинное значение")
 axes[0].set_title("Значение площади")
 axes[0].set_xlabel("Количество точек")
+axes[0].set_ylim(0.85, 1.05)
 axes[0].grid()
 axes[0].legend()
 
 # Second graph
-axes[1].plot(x, err, color="red")
+axes[1].plot(x, err1, color="red", lw=1,
+             label="Площадь области точек $S_{rec} = 4$")
+axes[1].plot(x, err2, color="turquoise", lw=1,
+             label="Площадь области точек $S_{rec} = 9$")
 axes[1].set_title("Относительная погрешность")
 axes[1].set_xlabel("Количество точек")
+axes[1].set_ylim(-0.005, 0.10)
 axes[1].grid()
+axes[1].legend()
 
 # Adjust spacing between plots
 plt.tight_layout()
